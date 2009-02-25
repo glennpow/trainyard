733fb98004e22c0887a91eb9d058cb029ab67140
glennpow <glennpow@gmail.com>
2009-02-25 19:56:01 +0100

    Fixed auth session

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -2,6 +2,6 @@ class UserSession < Authlogic::Session::Base
   after_create :create_user_session
   
   def create_user_session
-    self.user.create_session(self.controller.session)
+    self.user.create_session(UserSession.controller.session)
   end
 end
