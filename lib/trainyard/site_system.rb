7159f1c695b4683fc7f7b9291e7d81f1684fe816
glennpow <glennpow@gmail.com>
2009-03-09 13:23:01 +0100

    Moved domain code to site_system.rb

---
+++ lib/trainyard/site_system.rb
@@ -0,0 +1,28 @@
+module Trainyard
+  module SiteSystem
+    def current_site
+      return @current_site if defined?(@current_site)
+      @current_site = Configuration.sites.detect { |site| site[:domain] == request.domain } || Configuration.sites.first
+      logger.debug "Application domain: #{request.domain} (#{@current_site[:name]})"
+      @current_site
+    end
+
+    def site_name
+      current_site[:name]
+    end
+
+    def site_email
+      current_site[:email]
+    end
+  
+    def site_email_host
+      current_site[:email_host]
+    end
+
+    def self.included(base)
+      base.send :helper_method, :current_site, :site_name, :site_email, :site_email_host if base.respond_to? :helper_method
+    end
+  end
+end
+
+ActionController::Base.send(:include, Trainyard::SiteSystem) if defined?(ActionController::Base)
